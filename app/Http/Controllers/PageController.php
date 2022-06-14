0ceefca1e0dc0e46e78578f2eb5dbe4e4cf2f68d
Alexxboc <boccardi.alessandro@gmail.com>
2022-06-14 15:43:42 +0200

    create travel Model

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\travel;
+
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('welcome');
+    }
 }
